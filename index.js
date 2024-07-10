b0860a5f8618838cf8a086c6aeff2e83fb2f806b
hasanovtemur05 <hasanovtemur86@gmail.com>
2024-07-11 01:07:58 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+// 1 - masala
+// const doublecate = () => {
+//     let arr = [1,2,3,4,5,4,3]
+//     let new_arr = []
+//     let arr2 = arr.filter(item => arr.indexOf(item) !== arr.lastIndexOf(item))
+//     arr2.forEach(item => {
+//     if (!new_arr.includes(item)) {
+//         new_arr.push(item);
+//     }
+//     });
+
+//     console.log(new_arr); 
+// }
+// doublecate()
+
+
+
+// 2 - masala
+// function square(n) {
+//     let son = [1,2,3,4,5]
+//     for(let i = 0; i < son.length; i++){
+//         let count = Math.pow(son[i], 2)
+//         console.log(count);
+//     } 
+// }
+// square()
+
+
+
+// 3 - masala
+// const Musbat = () => {
+//     let son = [1, -4, 12, 0, -3, 29, -150]
+//     let result = son.filter(item => item > 0)
+//     console.log(result);
+//     let count = result.reduce((a,b) => a + b, 0)
+//     console.log(count);
+    
+// }
+// Musbat()
+
+
+
+// 4 - masala
+// function capitalIetter(n) {
+//     let str = "George Raymond Richard Martin"
+//     let result = str.split(" ")
+//     console.log(result);
+//     let count = result.map((item)=> item[0]).join("")
+//    console.log(count);
+// }
+// capitalIetter()
+
+
+
+// 5 - masala
+const litter = () => {
+    let str = "salom12"
+    for(let i = 0; i < str.length; i++){
+       console.log(str[i]);
+    }
+  
+}  
+litter() 
+
+
+
+// 6 - masala
+// function wordLength(n) {
+//     let str = "hello world"
+//     let arr = str.split(" ")
+//     let result = arr.map((item)=> item.length)
+//     console.log(result);
+// }
+// wordLength()
+
+
+
+// 7 - masala
+// const emptySpace = () => {
+//     let str = "salom dunyo" 
+//     let arr = str.split("")
+// if (!arr.includes(" ")) {
+//     console.log("true");
+// }else{
+//     console.log("false");
+// }
+// }
+// emptySpace()
+
+
+
+// 8 - masala
+// function arrObj(n) {
+//     let obj = {a: 2, b: 5, c: 7}
+//     let arr = []
+//     for (key in obj){
+//         let result = key += obj[key]
+//         arr.push(result)
+//     }
+//     console.log(arr);
+// }
+// arrObj()
+
